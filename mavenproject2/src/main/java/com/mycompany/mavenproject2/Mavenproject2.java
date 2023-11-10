c5aeff030851887fb95a315c0e712cd5647dd106
Ronoh <95380895+daniel-caleb@users.noreply.github.com>
2023-11-10 09:36:36 +0300

    mavenproject2

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Meshewa
+ */
+public class Mavenproject2 {
+
+//    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 6,7,8,9,10};
+//        for (int number : numbers) {
+//            System.out.println(number);
+//        }
+//    }
+//}
+      public static void main(String args[]){  
+   int arr[]={12,13,14,44, 50, 60, 70};   
+     System.out.println(arr);  
+   }  
+ } 

9c9e8ff90d6e5911267922f5f5941bb074fc106c
Ronoh <95380895+daniel-caleb@users.noreply.github.com>
2023-11-10 09:46:46 +0300

    mavenproject4

---
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject4;
+
+/**
+ *
+ * @author Meshewa
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+    char ch='U';    
+    switch(ch)  
+    {  
+        case 'A':   
+            System.out.println("Albino");  
+            break;  
+        case 'B':   
+            System.out.println("Blind");  
+            break;  
+        case 'C':   
+            System.out.println("Crooked");  
+            break;  
+        case 'D':   
+            System.out.println("Deaf");  
+            break;  
+        case 'U':   
+            System.out.println("Autism");  
+            break;    
+        default:   
+            System.out.println("Wrong Entry");  
+    }  
+}    
+}

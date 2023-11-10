a31cb3d435fc4f18c4e3a6bbafcafce0b5938b9f
Ronoh <95380895+daniel-caleb@users.noreply.github.com>
2023-11-10 09:47:48 +0300

    mavenproject6

---
+++ mavenproject6/src/main/java/com/mycompany/mavenproject6/Mavenproject6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject6;
+
+/**
+ *
+ * @author Meshewa
+ */
+public class Mavenproject6 {
+
+    public static void main(String[] args) {
+        System.out.println("This is \"Sydney\"");
+    }
+}
